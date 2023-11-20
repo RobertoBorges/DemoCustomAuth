3b84b2ee4907308bc01c9f3293b8eb7529533b65
Roberto Borges <roberto.borges@outlook.com>
2023-11-20 09:47:18 -0500

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using DemoCustomAuth.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoCustomAuth.Pages
+{
+    [MyAuth]
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Util/CustomAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace DemoCustomAuth.Util
+{
+    public class MyAuthAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //check access 
+            if (CheckPermissions(context))
+            {
+                //all good, add optional code if you want. Or don't
+            }
+            else
+            {
+                //DENIED!
+                //return "ChallengeResult" to redirect to login page (for example)
+                context.Result = new ChallengeResult(CookieAuthenticationDefaults.AccessDeniedPath);
+            }
+        }
+
+        private bool CheckPermissions(AuthorizationFilterContext context)
+        {
+            if(context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                //check if user is in role
+                if(context.HttpContext.User.IsInRole("Admin"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
